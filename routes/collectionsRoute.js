d13b4da622cf40eac35dd3df3dbfbc7ba289cc3f
matannturgeman <matannturgeman@gmail.com>
2020-03-10 03:26:42 +0200

    add routes to backend

---
+++ routes/collectionsRoute.js
@@ -0,0 +1,16 @@
+
+const fetch = require('node-fetch')
+const { SEARCH_QUERY_URL } = require('../urls/urls.json')
+
+const SEARCH_LIMIT = 25
+
+module.exports = app => {
+    app.post('/collections', (req, res) => {
+        let { query = '' } = req.body
+        query = query.replace(' ', '+')
+        fetch(`${SEARCH_QUERY_URL}${query}&limit=${SEARCH_LIMIT}`)
+            .then(res => res.json())
+            .then(data =>  res.json(data))
+            .catch(err => res.status(500).end())
+    })
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+const collectionsRoute = require('./routes/collectionsRoute')
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
+const express = require('express')
+const https = require('https');
+const cors = require('cors')
+const http = require('http');
+require('dotenv').config()
+
+const app = express();
+const port = 9000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+collectionsRoute(app);
+
+//insert ssl certificates for https
+const serverOptions = {}
+const isDev = process.env.ENV === 'dev'
+
+const server = isDev? http.createServer(app) :https.createServer(serverOptions, app)
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
